6ee0b8ecccf843072312f94e3826f2136568632c
Kaylene <carnavos@hawaii.edu>
2022-10-19 17:09:00 -1000

    Parts 1-9

--- HackwichSix/ViewController.swift
+++ HackwichSix/ViewController.swift
@@ -5,9 +5,31 @@
 //  Created by Kaylene Carnavos on 10/19/22.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var myFriendsArray = ["Katie", "Sam", "Clarissa"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        
+        return myFriendsArray.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellReuseIdentifier")
+        
+        let text = myFriendsArray[indexPath.row]
+        
+        cell?.textLabel?.text = text
+        
+        return cell!
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
